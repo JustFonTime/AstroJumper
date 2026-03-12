03fb744ca539766123c571cc1adc953221ee35e1
William Wu <wwu108@ucsc.edu>
2026-03-12 00:01:52 -0700

    Added animations to the player
    Most of the animations look good using the animation system built in unity
    Only the landing looks a little clunky(for now is good)

---
+++ Assets/Scripts/Ground/PlayerAnimator.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(GroundMovement), typeof(Rigidbody2D))]
+// mangages player animations based on movement and grounded state
+// uses the animator 
+// can make the landing animation more smooth but for now is good
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+    private static readonly int SpeedParam = Animator.StringToHash("Speed");
+    private static readonly int IsGroundedParam = Animator.StringToHash("IsGrounded");
+    private static readonly int YVelocityParam = Animator.StringToHash("YVelocity");
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Vector2 groundCheckSize = new Vector2(0.6f, 0.1f);
+    [SerializeField] private LayerMask groundMask;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        // Horizontal speed drives Idle from and to Walk
+        float horizontalSpeed = Mathf.Abs(rb.linearVelocity.x);
+        animator.SetFloat(SpeedParam, horizontalSpeed);
+
+        // Vertical velocity drives JumpRise to JumpFall
+        animator.SetFloat(YVelocityParam, rb.linearVelocity.y);
+
+        // Grounded check 
+        bool isGrounded = Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0f, groundMask);
+        animator.SetBool(IsGroundedParam, isGrounded);
+    }
+}
